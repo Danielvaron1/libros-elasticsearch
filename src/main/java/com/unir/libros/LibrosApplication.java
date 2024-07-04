0deb0c504d58f679cdb712a3e564234fe2858751
Danielvaron1 <dani-0153@hotmail.com>
2024-07-03 21:25:44 -0500

    railway

--- src/main/java/com/unir/libros/LibrosApplication.java
+++ src/main/java/com/unir/libros/LibrosApplication.java
@@ -1,12 +1,20 @@
 package com.unir.libros;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class LibrosApplication {
 
 	public static void main(String[] args) {
+		// Retrieve execution profile from environment variable. If not present, default profile is selected.
+		String profile = System.getenv("PROFILE");
+		System.setProperty("spring.profiles.active", profile != null ? profile : "default");
+		// Railway's internal interface takes some time to start. We wait for it to be ready.
+		log.debug("Waiting fo Internal Interface to start...");
+
 		SpringApplication.run(LibrosApplication.class, args);
 	}
 
